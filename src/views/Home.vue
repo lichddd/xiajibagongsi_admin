7bf1150cc789fbed519de0b8d4e5b43eadd7b150
lichddd <lichddd@126.com>
2017-09-28 08:19:04 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,11 +58,12 @@ export default {
     axios.get('admin/info').then(m => {
 
       this.info=m.data.info;
+          this.drawChart();
 
     });
     this.echart = echarts.init(document.getElementsByClassName('echart')[0]);
 
-    this.drawChart();
+
   },
   data () {
     return {
@@ -82,14 +83,86 @@ export default {
     drawChart(){
 
       // 绘制图表
-      this.echart.setOption();
+      this.echart.setOption(this.getData());
 
 
     }
     ,
     getData(){
 
-      return option;
+var date = [];
+var data = [];
+this.info.visit.visit.forEach((l)=>{
+
+  date.push(l.date);
+  data.push(l.count);
+})
+
+
+
+      return  {
+          tooltip: {
+              trigger: 'axis',
+              position: function (pt) {
+                  return [pt[0], '10%'];
+              }
+          },
+          title: {
+              left: 'center',
+              text: '每日访问量',
+          },
+          toolbox: {
+              feature: {
+                  dataZoom: {
+                      yAxisIndex: 'none'
+                  },
+                  restore: {},
+                  saveAsImage: {}
+              }
+          },
+          xAxis: {
+              type: 'category',
+              boundaryGap: false,
+              data: date
+          },
+          yAxis: {
+              type: 'value',
+              boundaryGap: [0, '100%']
+          },
+          dataZoom: [{
+              type: 'inside',
+              start: 0,
+              end: 2
+          }, {
+              start: 0,
+              end: 2,
+              handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+              handleSize: '50%',
+              handleStyle: {
+                  color: '#fff',
+                  shadowBlur: 3,
+                  shadowColor: 'rgba(0, 0, 0, 0.6)',
+                  shadowOffsetX: 2,
+                  shadowOffsetY: 2
+              }
+          }],
+          series: [
+              {
+                  name:'人数',
+                  type:'line',
+                  smooth:true,
+                  symbol: 'none',
+                  sampling: 'average',
+                  itemStyle: {
+                      normal: {
+                          color: 'rgb(255, 70, 131)'
+                      }
+                  },
+
+                  data: data
+              }
+          ]
+      };
     }
   },
   beforeDestroy(){
